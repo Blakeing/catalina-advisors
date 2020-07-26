38a09f7e2b6e7c319f0c7005db3c0b48e319dd89
Blake <blakeingenthron@gmail.com>
2020-07-26 16:40:57 -0500

    intial commit

---
+++ src/components/Marketing.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { Link as ChakraLink, Text, Box, Image } from "@chakra-ui/core"
+import plane from "../images/plane.png"
+
+export const Marketing = () => {
+  return (
+    <Box p={4} display={{ md: "flex" }}>
+      <Box flexShrink="0">
+        <Image
+          rounded="lg"
+          width={{ md: 40 }}
+          src={plane}
+          alt="Woman paying for a purchase"
+        />
+      </Box>
+      <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+        <Text
+          fontWeight="bold"
+          textTransform="uppercase"
+          fontSize="sm"
+          letterSpacing="wide"
+          color="teal.600"
+        >
+          Marketing
+        </Text>
+        <ChakraLink
+          mt={1}
+          display="block"
+          fontSize="lg"
+          lineHeight="normal"
+          fontWeight="semibold"
+          href="#"
+        >
+          Finding customers for your new business
+        </ChakraLink>
+        <Text mt={2} color="gray.500">
+          Getting a new business off the ground is a lot of hard work. Here are
+          six ideas you can use to find your first customers.
+        </Text>
+      </Box>
+    </Box>
+  )
+}
---
+++ src/pages/test.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { ChakraDrawer } from "../components/ChakraDrawer"
+import { useDisclosure, Button, Flex } from "@chakra-ui/core"
+import { DarkModeSwitch } from "../components/DarkModeSwitch"
+import { Marketing } from "../components/Marketing"
+import { Container } from "../components/Container"
+
+const Test = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return (
+    <Container>
+      <ChakraDrawer btnRef={btnRef} isOpen={isOpen} onClose={onClose} />
+      <Flex align="flex-start" py={2} pl={2} justify="flex-start" w="100%">
+        <Button ref={btnRef} variantColor="teal" onClick={onOpen}>
+          Open
+        </Button>
+      </Flex>
+
+      <DarkModeSwitch />
+      <Marketing />
+      <Marketing />
+    </Container>
+  )
+}
+
+export default Test
