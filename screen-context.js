74ddb12deb63c85e5ca0f455febb328d2359a205
Blake <blakeingenthron@gmail.com>
2020-08-02 16:44:21 -0500

    work

--- screen-context.js
+++ screen-context.js
@@ -5,17 +5,17 @@ const ScreenContext = React.createContext({})
 exports.ScreenContext = ScreenContext
 
 const ScreenProvider = props => {
-  const [windowDimensions, setWindowDimensions] = React.useState(
-    getWindowDimensions()
-  )
-
-  function getWindowDimensions() {
+  const getWindowDimensions = () => {
     const { innerWidth: width, innerHeight: height } = window
     return {
       width,
       height,
     }
   }
+
+  const [windowDimensions, setWindowDimensions] = React.useState(
+    getWindowDimensions()
+  )
 
   React.useEffect(() => {
     function handleResize() {
