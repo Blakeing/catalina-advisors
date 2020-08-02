97991a3d0443b24d30b5b3e6459750ce2fa0bb6b
Blake <blakeingenthron@gmail.com>
2020-08-02 14:30:56 -0500

    doe sit need srr?

---
+++ gatsby-ssr.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react"
+
+const WrapperComponent = props => {
+  useEffect(() => {
+    window.onresize = function () {
+      document.body.height = window.innerHeight
+    }
+
+    window.onresize() // called to initially set the height.
+  }, [])
+
+  return props.children
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <WrapperComponent>{element}</WrapperComponent>
+}
